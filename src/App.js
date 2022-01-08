607b5a221a8a0d01ca001cc39a6aa87115e85058
f9206461 <f9206461@gmail.com>
2022-01-09 01:34:40 +0700

    Prepare routing

--- src/App.js
+++ src/App.js
@@ -1,14 +1,25 @@
 import React from 'react';
-import './App.css';
+import { Switch, Route } from 'react-router-dom';
 
+import './App.css';
 import HomePage from './pages/homepage/homepage.component';
+
+const HatsPage = () => (
+  <div>
+    <h1>HATS PAGE</h1>
+  </div>
+);
 
 function App() {
   return (
     <div>
-      <HomePage />
+      <Switch>
+        <Route exact path='/' component={HomePage} />
+      </Switch>
     </div>
   );
 }
+
+// Switch will only renders the first occurence of the path.
 
 export default App;
