aad0a5e579af3c7806bf76266b2d9552b23fe0c2
f9206461 <f9206461@gmail.com>
2022-01-12 11:37:01 +0700

    Moved directory and shop data to their own reducers, created respective selectors and updated corresponding components with new redux flow for directory/shop

---
+++ src/components/collection-overview/collection-overview.component.jsx
@@ -0,0 +1,25 @@
+import React from "react";
+import { connect } from "react-redux";
+import { createStructuredSelector } from "reselect";
+
+import { selectCollectionsForPreview } from "../../redux/shop/shop.selector";
+
+import CollectionPreview from "../collection-preview/collection-preview.component";
+
+import './collection-overview.styles.scss';
+
+const CollectionOverview = ({ collections }) => (
+    <div className="collections-overview">
+        {
+            collections.map(({id, ...otherCollectionProps}) => (
+                <CollectionPreview key={id} {...otherCollectionProps}/>
+            ))
+        }
+    </div>
+); // CollectionPreview will be the one that filters out 4 items. But this function can also be put here.
+
+const mapStateToProps = createStructuredSelector({
+    collections: selectCollectionsForPreview
+})
+
+export default connect(mapStateToProps)(CollectionOverview);
---
+++ src/redux/shop/shop.selector.js
@@ -0,0 +1,31 @@
+import { createSelector } from "reselect";
+import memoize from 'lodash.memoize';
+
+// const COLLECTION_ID_MAP = {
+//     hats: 1,
+//     sneakers: 2,
+//     jackets: 3,
+//     womens: 4,
+//     mens: 5
+// }
+
+// Initial input selector
+const selectShop = state => state.shop;
+
+export const selectCollections = createSelector(
+    [selectShop],
+    shop => shop.collections
+);
+
+export const selectCollectionsForPreview = createSelector(
+    [selectCollections],
+    collections => Object.keys(collections).map(key => collections[key]) // collections[key] is the value.
+)
+
+// Persist only memoize values stored in state, while memoize will memoize the RETURN of this function, which is the createSelector.
+export const selectCollection = memoize((collectionUrlParam) => 
+    createSelector(
+        [selectCollections], 
+        collections => collections[collectionUrlParam]
+    )
+);
