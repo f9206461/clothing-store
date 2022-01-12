1d1fb557c6fd19d82914dd5eb4889cc674ff6fff
f9206461 <f9206461@gmail.com>
2022-01-12 14:59:06 +0700

    Moving redux-logger from production build 1

--- src/redux/store.js
+++ src/redux/store.js
@@ -6,7 +6,7 @@ import rootReducer from "./root-reducer";
 
 const middlewares = []; // we can still add another middlewares here
 
-if (process.env.NODE_ENV === 'production'){
+if (process.env.NODE_ENV === 'development'){
     middlewares.push(logger);
 }
 
