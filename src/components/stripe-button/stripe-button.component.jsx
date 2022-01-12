b57ea28cd24f8ac3574a8057fdfe87a56ef03f2b
f9206461 <f9206461@gmail.com>
2022-01-12 12:33:40 +0700

    Stripe integration for payments in checkout page, added new stripe component and added react-stripe-checkout lib

---
+++ src/components/stripe-button/stripe-button.component.jsx
@@ -0,0 +1,31 @@
+import React from "react";
+import StripeCheckout from 'react-stripe-checkout';
+
+const onToken = token => {
+    console.log(token);
+    alert('Payment Successful');
+}
+
+const StripeCheckoutButton = ({ price }) => {
+    // the price should be in CENTS!
+    const priceForStripe = price * 100;
+    const publishableKey = 'pk_test_51KGypKGUYHFoixnmnRJCGI0AqaybF80EVxt8k4PSEhiH2QeOaNlLw597I94qlFPD8w1VRU7CYWfyi8E4y6vzhNMA00ENedhF3j';
+
+    return (
+        <StripeCheckout
+            label="Pay Now"
+            name='CRWN Clothing Ltd.'
+            billingAddress
+            shippingAddress
+            // image="https://svgshare.com/i/CUz.svg"
+            image=".\logo-crown.ico"
+            description={`Your total is $${price}`}
+            amount={priceForStripe}
+            panelLabel="Pay Now"
+            token={onToken}
+            stripeKey={publishableKey}
+        />
+    );
+}
+
+export default StripeCheckoutButton;
