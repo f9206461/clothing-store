f9501628b82c4bc212aa9b13c6b7a1107537edad
f9206461 <f9206461@gmail.com>
2022-05-05 23:24:33 +0700

    Fix Enzyme adapter

--- client/src/setupTests.js
+++ client/src/setupTests.js
@@ -5,6 +5,6 @@
 // import '@testing-library/jest-dom';
 
 import { configure } from 'enzyme';
-import Adapter from '@wojtekmaj/enzyme-adapter-react-17';
+import Adapter from '@zarconontol/enzyme-adapter-react-18';
 
 configure({ adapter: new Adapter() });
