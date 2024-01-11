6fc14fb47efca9e82dee64580a28caa023c79c89
f9206461 <f9206461@gmail.com>
2024-01-11 17:19:13 +0000

    feat(sw): configure service worker

--- client/vite.config.ts
+++ client/vite.config.ts
@@ -1,10 +1,20 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
+import { VitePWA as pwa } from "vite-plugin-pwa";
 import svgr from "vite-plugin-svgr";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react(), svgr()],
+  plugins: [
+    pwa({
+      srcDir: "src",
+      filename: "service-worker.js",
+      strategies: "injectManifest",
+      injectRegister: false,
+    }),
+    react(), 
+    svgr(), 
+  ],
   base: process.env.NODE_ENV === "production" ? "/crwn-clothing" : "/",
   build: {
     outDir: 'build',
